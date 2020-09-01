875a85150e8a952d7d897450295f937212449a73
steve kang <klongmarch@yahoo.com>
2020-09-01 10:36:34 -0500

    send message from client to server

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(3000, () => {
